d53db5556f8ea5b7ca84fe0f0a7dcd96949c6657
adilzulfiqar <adil.muhammad195@gmail.com>
2020-02-04 01:25:55 +0500

    commit

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import Loader from '../../common/Loader/Loader.js';
-import SpeedTest from '../../SpeedTest';
-// const SpeedTest = React.lazy(() => import('../../SpeedTest'));
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Loader from '../../common/Loader/Loader.js';
+const SpeedTest = React.lazy(() => import('../../SpeedTest'));
 
 function App() {
-  return <SpeedTest />;
+  return (
+    <Router>
+      <React.Suspense fallback={<Loader />}>
+        <Route path='/' exact>
+          <SpeedTest />
+        </Route>
+      </React.Suspense>
+    </Router>
+  );
 }
 
-// {/* <Router>
-//   <React.Suspense fallback={<Loader />}>
-//     <Route path='/' exact>
-//     </Route>
-//   </React.Suspense>
-// </Router> */}
 export default App;
