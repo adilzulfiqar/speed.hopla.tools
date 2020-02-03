df1cdb44e50aecb56997ec39d1b73921c1d3aae3
adilzulfiqar <adil.muhammad195@gmail.com>
2020-02-04 00:58:01 +0500

    commit

--- server/index.js
+++ server/index.js
@@ -5,24 +5,24 @@ const port = process.env.PORT || 5000;
 
 // app.get('/', (req, res) => res.send('Hello World!'));
 
-var speedTest = require('speedtest-net');
-var test = speedTest({ maxTime: 5000 });
+// var speedTest = require('speedtest-net');
+// var test = speedTest({ maxTime: 5000 });
 
-test.on('downloadspeedprogress', speed => {
-  console.log(
-    'Download speed (in progress):',
-    (speed * 125).toFixed(2),
-    'KB/s'
-  );
-});
+// test.on('downloadspeedprogress', speed => {
+//   console.log(
+//     'Download speed (in progress):',
+//     (speed * 125).toFixed(2),
+//     'KB/s'
+//   );
+// });
 
-speedTest.visual({ maxTime: 5000 }, (err, data) => {
-  console.dir(data);
-});
+// speedTest.visual({ maxTime: 5000 }, (err, data) => {
+//   console.dir(data);
+// });
 
-test.on('error', err => {
-  console.error(err);
-});
+// test.on('error', err => {
+//   console.error(err);
+// });
 
 app.use(express.static(path.resolve(__dirname, '../build')));
 
